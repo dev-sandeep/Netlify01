326e5c79da6cbeb889824f492186e8e640b87a43
Sandeep G <dev.sandeep@hotmail.com>
2024-02-10 21:42:21 +0530

    updating text

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React on 10.02.2024
-        </a>
+        <h2>We are awesome students!</h2>
       </header>
     </div>
   );
